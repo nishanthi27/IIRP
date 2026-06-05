65f90850e88ef5da435631b5710d6c139f736625
nishanthi27 <nishanthiganesh2003@gmail.com>
2026-06-05 19:06:10 +0530

    added severity in prediction.py

--- app/test_prediction.py
+++ app/test_prediction.py
@@ -3,13 +3,18 @@ import pickle
 with open("models/vectorizer.pkl", "rb") as file:
     vectorizer = pickle.load(file)
 
-with open("models/model.pkl", "rb") as file:
-    model = pickle.load(file)
+with open("models/category_model.pkl", "rb") as file:
+    model_category = pickle.load(file)
+
+
+with open("models/severity_model.pkl", "rb") as file:
+    model_severity = pickle.load(file)
 
 incident = "unauthorized login attempt"
 
 incident_vector = vectorizer.transform([incident])
 
-prediction = model.predict(incident_vector)
+prediction_category = model_category.predict(incident_vector)
+prediction_severity = model_severity.predict(incident_vector)
 
-print(prediction[0])
+print(prediction_category,prediction_severity)
